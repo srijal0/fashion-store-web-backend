fe1ca157564a57d33d41e304fdca14aba6a589f0
Shreejal Shrestha <shresthashreejal84@gmail.com>
2026-01-04 18:38:08 +0545

    first commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 // Info: Application level constant, with fallbacks
 // Info: if .env variables are not set
-export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000; 
-export const MONGODB_URI: string = process.env.MONGODB_URI || "mongodb://localhost:27017/local-veda-verse_db";
+export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5000; 
+export const MONGODB_URI: string = process.env.MONGODB_URI || "mongodb://localhost:27017/local-fashion-store_db";
 export const JWT_SECRET:string = process.env.JWT_SECRET || "secret_key";
